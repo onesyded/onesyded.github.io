72c03db67407c1651893094ffca3f1c7fd31c796
onesyded <richmond006mensah@gmail.com>
2025-10-19 17:20:44 +0000

    first push

---
+++ scripts/inedx.js
@@ -0,0 +1,13 @@
+let firstname = "Richmond"
+let lastname = " Mensah"
+let fullname = firstname + lastname;
+console.log(fullname)
+lastname = 5;
+function Add(x,y) {
+return x + y;
+    
+}
+let res = Add(2,5);
+console.log(res)
+let sentence = `${firstname} ${lastname}`
+console.log(sentence)
